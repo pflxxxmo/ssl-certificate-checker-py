c5a840ac020f9ba5430cfd30c12a4ab79fbac649
Dzmitry_Asetnik <Dzmitry_Asetnik@epam.com>
2021-12-16 12:06:06 +0300

    fixed exception, add logging and redesigned check_certificate function

--- README.md
+++ README.md
@@ -8,12 +8,17 @@ This module was created to check SSL certificates at a given URL and notify you 
 
 In this module, the following libraries were used:
 ```
-import ssl
-from time import time
-from datetime import datetime
+from datetime import datetime, timedelta
+from os import environ
+from ssl import get_server_certificate
 from urllib.parse import urlparse
-from pathlib import Path
+from logging import error, basicConfig, INFO, info
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+
 import telebot
+from telebot.types import Message
 ```
 You also need to find a bot in the Telegram named **[SSL Certificate Checker](https://t.me/SSL_Certificate_CheckerBot)**
 
--- main.py
+++ main.py
@@ -1,44 +1,69 @@
-import ssl
-import os
+from datetime import datetime, timedelta
+from os import environ
+from ssl import get_server_certificate
+from urllib.parse import urlparse
+from logging import error, basicConfig, INFO, info
+
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-from datetime import datetime, timedelta
-from urllib.parse import urlparse
+
 import telebot
+from telebot.types import Message
 
 # access token for telegram bot
-access_token = os.environ.get('API_KEY')
+access_token = environ.get('API_KEY')
 # the time to notify of the expiration of the certificate(days)
 notice_date = 10
 bot = telebot.TeleBot(access_token)
 
 
-def check_certificate(url: str):
+def check_certificate(url: str) -> tuple:
+    """Takes one parameter of type string, returns a tuple with values of type bool and datetime.datetime.
+    This function parses and decrypts PEM certificate."""
+
     parsed_url = urlparse(url)
     try:
-        cert = ssl.get_server_certificate(
+        cert = get_server_certificate(
             (parsed_url.hostname, parsed_url.port or 443))
-    except Exception:
-        return "Bad request"
-    try:
-        parsed_cert = x509.load_pem_x509_certificate(str.encode(cert), default_backend())
-    except Exception:
-        return
-    date = parsed_cert.not_valid_after
-    if date < datetime.today() or (date - datetime.today()) <= timedelta(notice_date):
-        return f'ALARM‼️. The certificate for url {url} was expired in {date}'
+    except:
+        error('Name or service not known. Status: ERROR')
+        return None, False
+    parsed_cert = x509.load_pem_x509_certificate(
+        str.encode(cert), default_backend())
+    info(f'The certificate for the URL has been received:{url}. Status: INFO')
+    if parsed_cert.not_valid_after < datetime.today() or (parsed_cert.not_valid_after - datetime.today()) <= timedelta(notice_date):
+        return parsed_cert.not_valid_after, True
     else:
-        return f'OK🆗.The certificate for url {url} expires in {date}'
+        return parsed_cert.not_valid_after, False
+
+
+def prepare_for_response(url: str) -> str:
+    """The function takes a value of type str and returns a value of type str. 
+    This function takes an address, sends it for checking and as a result gives a string for the answer. """
+
+    date, result = check_certificate(url)
+    if date == None and result == False:
+        return 'The certificate at this URL does not exist'
+    else:
+        if result:
+            return f'ALARM‼️. The certificate for url {url} was expired in {date}'
+        else:
+            return f'OK🆗.The certificate for url {url} expires in {date}'
 
 
 @bot.message_handler(content_types=['text'])
-def response(message):
+def response(message: Message):
+    """The function takes a message of type telebot.Message.
+    This function is responsible for finding the string entered by the bot and sending it for processing.
+    This function also allows the bot to send a message with the result of processing"""
+
     if str(message.text).startswith("https://"):
         url = message.text
-        bot.send_message(message.chat.id, check_certificate(url))
+        bot.send_message(message.chat.id, prepare_for_response(url))
     else:
         url = "https://" + message.text
-        bot.send_message(message.chat.id, check_certificate(url))
+        bot.send_message(message.chat.id, prepare_for_response(url))
 
 
+basicConfig(format='%(asctime)s - %(message)s', level=INFO)
 bot.polling(none_stop=True)
